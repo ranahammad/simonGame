0decb840c94841d086adbe5e4ed80620015753c3
Rana Hammad <rana.hammad@live.com>
2012-03-11 21:42:23 +0500

    simon Game project files

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  simonGame
+//
+//  Created by Gobbledygook on 10/23/10.
+//  Copyright TelephonyMedia 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
